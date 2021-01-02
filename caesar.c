cccb5df61a5944ca061366c8e3bd3a857e5a918d
ymsht <git@ymsht.info>
2021-01-02 22:54:36 +0900

    合同算術に変更

--- caesar.c
+++ caesar.c
@@ -3,7 +3,25 @@
 
 #include "caesar.h"
 
-#include <stdio.h>
+static const char LARGE_CHAR_TABLE[] = {
+    'a', 'b', 'c', 'd', 'e',
+    'f', 'g', 'h', 'i', 'j',
+    'k', 'l', 'm', 'n', 'o',
+    'p', 'q', 'r', 's', 't',
+    'u', 'v', 'w', 'x', 'y',
+    'z'
+};
+
+static const char SMALL_CHAR_TABLE[] = {
+    'A', 'B', 'C', 'D', 'E',
+    'F', 'G', 'H', 'I', 'J',
+    'K', 'L', 'M', 'N', 'O',
+    'P', 'Q', 'R', 'S', 'T',
+    'U', 'V', 'W', 'X', 'Y',
+    'Z'
+};
+
+static const int ALPHABET_NUM = 26;
 
 /*
  * シーザー暗号による暗号化を行った暗号文を返します
@@ -14,14 +32,29 @@
  */
 char* caesar(int shiftNum, char* plaintext) {
     int i;
-    int characterCode;
+    int j;
+    int k;
     int length = strlen(plaintext);
+    int index = -1;
+    int shiftIndex;
     char *ciphertext = (char *)malloc(sizeof(char) * (length + 1));
 
-    // 文字→数字化
     for (i = 0; i < length; i++) {
-        characterCode = plaintext[i];
-        ciphertext[i] = characterCode + shiftNum;
+        for (j = 0; j < ALPHABET_NUM; j++) {
+            if (plaintext[i] == LARGE_CHAR_TABLE[j]) {
+                shiftIndex = (j + shiftNum) % ALPHABET_NUM;
+                ciphertext[i] = LARGE_CHAR_TABLE[shiftIndex];
+                continue;
+            }
+        }
+
+        for (k = 0; k < ALPHABET_NUM; k++) {
+            if (plaintext[i] == SMALL_CHAR_TABLE[k]) {
+                shiftIndex = (k + shiftNum) % ALPHABET_NUM;
+                ciphertext[i] = SMALL_CHAR_TABLE[shiftIndex];
+                continue;
+            }
+        }
     }
 
     ciphertext[length] = 0;
