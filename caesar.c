96f4a7d3c1ea17622e8a69e3113dd4d376ad4c07
ymsht <git@ymsht.info>
2021-01-03 19:18:36 +0900

    asciiコードに依存した実装に変更

--- caesar.c
+++ caesar.c
@@ -3,24 +3,6 @@
 
 #include "caesar.h"
 
-static const char LARGE_CHAR_TABLE[] = {
-    'a', 'b', 'c', 'd', 'e',
-    'f', 'g', 'h', 'i', 'j',
-    'k', 'l', 'm', 'n', 'o',
-    'p', 'q', 'r', 's', 't',
-    'u', 'v', 'w', 'x', 'y',
-    'z'
-};
-
-static const char SMALL_CHAR_TABLE[] = {
-    'A', 'B', 'C', 'D', 'E',
-    'F', 'G', 'H', 'I', 'J',
-    'K', 'L', 'M', 'N', 'O',
-    'P', 'Q', 'R', 'S', 'T',
-    'U', 'V', 'W', 'X', 'Y',
-    'Z'
-};
-
 static const int ALPHABET_NUM = 26;
 
 /*
@@ -32,34 +14,28 @@ static const int ALPHABET_NUM = 26;
  */
 char* encryption(int shiftNum, char* plaintext) {
     int i;
-    int j;
-    int k;
     int length = strlen(plaintext);
     int index = -1;
     int shiftIndex;
     char *ciphertext = (char *)malloc(sizeof(char) * (length + 1));
 
     for (i = 0; i < length; i++) {
-        if (plaintext[i] < 'a' || plaintext[i] > 'z') {
-            // 暗号化対象はa-zA-Z
-            ciphertext[i] = plaintext[i];
+        // a-z
+        if (plaintext[i] >= 'a' && plaintext[i] <= 'z') {
+            shiftIndex = (plaintext[i] - 'a' + shiftNum) % ALPHABET_NUM;
+            ciphertext[i] = 'a' + shiftIndex;
             continue;
         }
-        for (j = 0; j < ALPHABET_NUM; j++) {
-            if (plaintext[i] == LARGE_CHAR_TABLE[j]) {
-                shiftIndex = (j + shiftNum) % ALPHABET_NUM;
-                ciphertext[i] = LARGE_CHAR_TABLE[shiftIndex];
-                continue;
-            }
+
+        // A-Z
+        if (plaintext[i] >= 'A' && plaintext[i] <= 'Z') {
+            shiftIndex = (plaintext[i] - 'A' + shiftNum) % ALPHABET_NUM;
+            ciphertext[i] = 'A' + shiftIndex;
+            continue;
         }
 
-        for (k = 0; k < ALPHABET_NUM; k++) {
-            if (plaintext[i] == SMALL_CHAR_TABLE[k]) {
-                shiftIndex = (k + shiftNum) % ALPHABET_NUM;
-                ciphertext[i] = SMALL_CHAR_TABLE[shiftIndex];
-                continue;
-            }
-        }
+        // 暗号化対象はa-zA-Z
+        ciphertext[i] = plaintext[i];
     }
 
     ciphertext[length] = 0;
--- caesar.h
+++ caesar.h
@@ -6,3 +6,12 @@
  * @return char* 暗号文
  */
 char* encryption(int shiftNum, char* plaintext);
+
+/*
+ * シーザー暗号による復号を行った平文を返します
+ *
+ * @param int shiftNum シフト回数
+ * @param char* ciphertext 暗号文
+ * @return char* 平文
+ */
+char* decryption(int shiftNum, char* ciphertext);
